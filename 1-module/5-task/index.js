1fb53367726415153c79b6546e916c58061c3f27
LadyAnanda <ladyananda@yandex.ru>
2023-01-24 12:35:42 +0300

    homework 1.2 , 1.4, 1.5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,10 @@
+const strConst = "Вот, что мне хотелось бы сказать на эту тему:";
+const maxlengthConst = 20;
+
 function truncate(str, maxlength) {
-  // ваш код...
+  return str.length > maxlength ? str.substring(0, maxlength - 1) + "…" : str;
 }
+
+truncate(strConst, maxlengthConst);
+console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
+console.log(truncate("Всем привет!", 20));
