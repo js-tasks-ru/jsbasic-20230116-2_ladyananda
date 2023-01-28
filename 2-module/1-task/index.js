1f941020437e404d4b43572caaaaf2b37b479e8b
LadyAnanda <ladyananda@yandex.ru>
2023-01-28 22:09:14 +0300

    homework 2.1, 2.2 , 2,3

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,19 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: "December",
+  currency: "USD",
+  isPayed: false,
+};
+
+function sumSalary(salaryObject) {
+  let sum = 0;
+  for (let key in salaryObject) {
+    if (!isNaN(salaryObject[key]) && isFinite(salaryObject[key])) {
+      sum += salaryObject[key];
+    }
+  }
+  return sum;
 }
+console.log(sumSalary(salaries));
