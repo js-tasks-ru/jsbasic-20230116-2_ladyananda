ec4951c77d2c77a64c46d51a4846f66596e7636a
LadyAnanda <ladyananda@yandex.ru>
2023-02-07 16:28:07 +0300

    Merge branch 'master' of https://github.com/js-tasks-ru/jsbasic-20230116-2_ladyananda

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,21 @@
+let result = {
+  min: -5.8,
+  max: 73,
+};
+
+let inputData = "1 и -5.8 или 10 хотя 34 + -5.3 и 73";
+
 function getMinMax(str) {
-  // ваш код...
+  let array = str.split(/[ ,]+/);
+
+  let minV = +array[0];
+  let maxV = minV;
+  for (let i = 0; i < array.length; i++) {
+    let e = +array[i];
+    if (e < minV) minV = e;
+    if (e > maxV) maxV = e;
+  }
+  return { min: minV, max: maxV };
 }
+
+console.log(getMinMax(inputData)); // { min: -5.8, max: 73  }
