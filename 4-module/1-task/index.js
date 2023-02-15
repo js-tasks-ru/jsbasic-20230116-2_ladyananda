2c27e825ed2455e9f6940bf9e51515a324d566c9
LadyAnanda <ladyananda@yandex.ru>
2023-02-15 11:51:56 +0300

    homework 4.1, 4.2, 4.3

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const $newUl = document.createElement("ul");
+
+  friends.forEach(function (item, i, arr) {
+    const $newLi = document.createElement("li");
+    $newLi.innerHTML = item.firstName + " " + item.lastName;
+    $newUl.appendChild($newLi);
+  });
+  return $newUl;
 }
